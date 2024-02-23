d4e5ad7c38c021a7298965dfb9f3fdeb11bba4bc
Jenny <jenny@redsparkdigital.co.uk>
2024-02-23 11:10:19 +0000

    adding discount function

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 require_once 'src/SimpleProduct.php';
 
 $coat = new SimpleProduct('Rain Coat', 'A colourful raincoat to keep you dry in the worst of the British weather',23.50 );
-$jumper = new SimpleProduct('Red Jumper', 'This cosy sweater is perfect mix of traditional style and modern tailoring.',34.99 );
+$jumper = new SimpleProduct('Red Jumper', 'This cosy sweater is perfect mix of traditional style and modern tailoring.',34.99, 10 );
 
 echo $coat->getDisplay();
 echo $jumper->getDisplay();
+
 
